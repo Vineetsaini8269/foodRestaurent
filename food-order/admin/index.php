be58ac75a1583961f65f43eac027959c59394ae9
VINEET SAINI <111350642+Vineetsaini8269@users.noreply.github.com>
2024-04-17 10:05:57 +0530

    Add files via upload
    food restaurent website

---
+++ food-order/admin/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Food Order Website - Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+
+     <!--menu Section starts -->
+     <div class="menu text-center">
+        <div class="wrapper">
+        <ul>
+         <li><a href ="#">home</a></li>
+         <li><a href ="#">Adimn manager</a></li>
+         <li><a href ="#">category</a></li>
+         <li><a href ="#">food</a></li>
+         <li><a href ="#">order</a></li>
+        </ul>
+        </div>
+        
+     </div>
+     <!--menu Section ends -->
+    
+
+     <!--main content  Section starts -->
+     <div class ="main-content">
+         <div class="wrapper">
+         main content Goes Here
+        </div>
+        
+     </div>
+     <!--maincontent Section ends -->
+
+
+
+     <!--footer Section starts -->
+     <div class ="footer">
+        <div class="wrapper">
+
+          <p class ="text-center"> 2024 All rights , FOOD order Resturent , Developed by - Vineet Saini </p>
+         
+        </div>
+        
+     </div>
+     <!--footer Section ends -->
+     
+
+</body>
+</html><!-- -->
